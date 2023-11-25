c2e236ad70fc0d9d4ee46b937fea94591ea45211
XpysTEEAM <nikolaev-evgenij@bk.ru>
2023-11-25 12:48:57 +0300

    Plasma added, started sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
-import "./App.css";
+import { Container } from "@salutejs/plasma-ui/components/Grid";
+import { Button } from "@salutejs/plasma-ui";
+
+import styles from "./App.module.css";
+import LeftSidebar from "./components/LeftSidebar/LeftSidebar";
 
 function App() {
-   return <div></div>;
+   return (
+      <main className={styles.main}>
+         <LeftSidebar />
+         <Container>
+            <Button>Hello, Plasma!</Button>
+         </Container>
+      </main>
+   );
 }
 
 export default App;
---
+++ src/components/LeftSidebar/LeftSidebar.tsx
@@ -0,0 +1,7 @@
+import styles from "./LeftSidebar.module.css";
+
+const LeftSidebar = () => {
+   return <section className={styles}>sidebar</section>;
+};
+
+export default LeftSidebar;
